51b2cfa88aa7b0a47874bb17919e1b1ac6631ac9
Henrry Cordovillo <henrry.cordovillo@epn.edu.ec>
2023-05-17 22:35:09 -0500

    Finalizacion de la seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
+/* orden de las importaciones
+  primero: Importaciones de javaScript
+  segundo: Importaciones de angular oficial
+  tercero: Librerias de terceros
+  cuarto: nuestro codigo
+
+*/
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CounterModule, HeroesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
